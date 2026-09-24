462e5d4feb2d14ab643b8096f3656b1db32137aa
Daniel Brill <179643835+DanielBrill20@users.noreply.github.com>
2026-09-23 23:12:32 -0400

    Added triangles to shapes.js

--- shapes.js
+++ shapes.js
@@ -24,6 +24,21 @@ const cubeEdges = [
     [3, 7],
 ];
 
+const cubeTriangles = [
+    [0, 2, 3],
+    [0, 3, 1],
+    [4, 5, 7],
+    [4, 7, 6],
+    [0, 4, 6],
+    [0, 6, 2],
+    [1, 3, 7],
+    [1, 7, 5],
+    [0, 1, 5],
+    [0, 5, 4],
+    [2, 6, 7],
+    [2, 7, 3]
+];
+
 const pyramid4Verts = [
     {x: -1,  y:  0,  z: -1},
     {x: -1,  y:  0,  z:  1},
@@ -43,6 +58,15 @@ const pyramid4Edges = [
     [3, 4]
 ];
 
+const pyramid4Triangles = [
+    [0, 1, 4],
+    [1, 2, 4],
+    [2, 3, 4],
+    [3, 0, 4],
+    [0, 2, 1],
+    [0, 3, 2]
+];
+
 const pyramid3Verts = [
     {x: -1,  y:  0,  z: -1},
     {x:  1,  y:  0,  z: -1},
@@ -59,8 +83,15 @@ const pyramid3Edges = [
     [2, 3]
 ];
 
+const pyramid3Triangles = [
+    [0, 1, 3],
+    [1, 2, 3],
+    [2, 0, 3],
+    [0, 1, 2]
+];
+
 export const shapes = {
-    cube: {vertices: cubeVerts, edges: cubeEdges},
-    pyramid4: {vertices: pyramid4Verts, edges: pyramid4Edges},
-    pyramid3: {vertices: pyramid3Verts, edges: pyramid3Edges}
+    cube: {vertices: cubeVerts, edges: cubeEdges, triangles: cubeTriangles},
+    pyramid4: {vertices: pyramid4Verts, edges: pyramid4Edges, triangles: pyramid4Triangles},
+    pyramid3: {vertices: pyramid3Verts, edges: pyramid3Edges, triangles: pyramid3Triangles}
 };
